54a485f651c6b0d1571aa8de57f648dbbc9a2032
Fufa Wakgari <fufi3934@gmail.com>
2023-04-18 23:34:47 +0300

    second commit

---
+++ lib/screens/add_task_screen.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class AddTaskScreen extends StatelessWidget {
+  const AddTaskScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       color: const Color(0xff757575),
+      child: Container(
+        decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+                topRight: Radius.circular(20),
+                topLeft: Radius.circular(20),
+            ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 32),
+          child: Column(
+           crossAxisAlignment: CrossAxisAlignment.start,
+            children:  [
+              const Text(
+                  'Add Task',
+                style: TextStyle(
+                  color: Color(0xFFc3700b3),
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              const SizedBox(height: 10,),
+             const TextField(
+
+              decoration: InputDecoration(
+                border: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Color(0xFFc3700b3),width: 2),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color:  Color(0xFFc3700b3),width: 2),
+                ),
+
+              ),
+            ),
+              const SizedBox(height: 60,),
+              Container(
+                width: 400,
+                height: 50,
+                decoration: const BoxDecoration(
+                  color:  Color(0xFFc3700b3),
+
+                ),
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'Add',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              )
+
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,22 +1,20 @@
 import 'package:flutter/material.dart';
 
 import '../widgets/tasks_list.dart';
+import 'add_task_screen.dart';
 
-class TasksScreen extends StatefulWidget {
+class TasksScreen extends StatelessWidget {
 
-  @override
-  State<TasksScreen> createState() => _TasksScreenState();
-}
 
-class _TasksScreenState extends State<TasksScreen> {
-  bool isChecked = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const  Color(0xFFc3700b3),
       floatingActionButton: FloatingActionButton(
         backgroundColor:  const Color(0xFFc3700b3),
-        onPressed: (){},
+        onPressed: (){
+          showModalBottomSheet(context: context, builder: (context)=>const AddTaskScreen());
+        },
         child: const Icon(
           Icons.add,
         ),
@@ -29,7 +27,8 @@ class _TasksScreenState extends State<TasksScreen> {
                 top: 60.0,
                 left: 30.0,
                 right: 30.0,
-                bottom: 30.0),
+                bottom: 30.0
+            ),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children:  const [
